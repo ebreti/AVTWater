e7ac912a6b53943415666488af13debfc41d89c4
Flavio M Carvalho <contato.ebreti@gmail.com>
2014-11-11 19:21:02 -0200

    Some help
    and a link to Readme.md

---
+++ lightDoc.md
@@ -0,0 +1,10 @@
+
+Choose water meter  
+and press `Show Graph`  
+
+See water consumption  
+by households in  
+Associação Vila Tijuca  
+  
+  
+See more [here](http://github.com/ebreti/AVTWater/blob/master/Readme.md).  
--- ui.R
+++ ui.R
@@ -1,4 +1,6 @@
 library(shiny)
+library(markdown)
+
 shinyUI(pageWithSidebar(
     headerPanel("Water Consumption by Households in AVT"),
     sidebarPanel(
@@ -9,7 +11,10 @@ shinyUI(pageWithSidebar(
                               "house#15", "house#16", "house#17", "house#18", "house#19", 
                               "house#20", "house#21", "common"),
                     selected="avt"),
-        submitButton("Show Graph")
+        submitButton("Show Graph"),
+        br(),
+        br(),
+        includeMarkdown("lightDoc.md")
     ),
     mainPanel(
         plotOutput("graph1")
